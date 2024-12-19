f17d544958311a9925a3858014911d8296d6c50a
alex <proto.merkabah@gmail.com>
2024-12-18 22:54:50 -0500

    nvim-cmp -> blink

--- nvim/lua/plugins/autocomplete.lua
+++ nvim/lua/plugins/autocomplete.lua
@@ -1,39 +1,47 @@
 return {
-  'hrsh7th/nvim-cmp',
-  dependencies = {
-    'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-buffer',
-    'hrsh7th/cmp-vsnip',
-    'hrsh7th/vim-vsnip',
+  'saghen/blink.cmp',
+  -- optional: provides snippets for the snippet source
+  dependencies = 'rafamadriz/friendly-snippets',
+
+  -- use a release tag to download pre-built binaries
+  version = 'v0.*',
+  -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+  -- build = 'cargo build --release',
+  -- If you use nix, you can build from source using latest nightly rust with:
+  -- build = 'nix run .#build-plugin',
+
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = {
+    -- 'default' for mappings similar to built-in completion
+    -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+    -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+    -- see the "default configuration" section below for full documentation on how to define
+    -- your own keymap.
+    keymap = { preset = 'super-tab' },
+
+    appearance = {
+      -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+      -- Useful for when your theme doesn't support blink.cmp
+      -- will be removed in a future release
+      use_nvim_cmp_as_default = true,
+      -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+      -- Adjusts spacing to ensure icons are aligned
+      nerd_font_variant = 'mono'
+    },
+
+    -- default list of enabled providers defined so that you can extend it
+    -- elsewhere in your config, without redefining it, via `opts_extend`
+    sources = {
+      default = { 'lsp', 'path', 'snippets', 'buffer' },
+      -- optionally disable cmdline completions
+      -- cmdline = {},
+    },
+
+    -- experimental signature help support
+    -- signature = { enabled = true }
   },
-  config = function() 
-    local cmp = require('cmp')
-    cmp.setup({
-      snippet = {
-      -- REQUIRED - you must specify a snippet engine
-        expand = function(args)
-          vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users
-        end,
-      },
-      window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered()
-      },
-      sources = cmp.config.sources({
-        { name = 'path' }, -- autocomplete file paths
-        { name = 'nvim_lsp' }, -- language server suggestions
-        { name = 'vsnip' },
-        },
-        {{ name = 'buffer' }, -- words in buffer
-      }),
-      mapping = cmp.mapping.preset.insert({
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.abort(),
-        -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-        ['<Tab>'] = cmp.mapping.confirm({ select = true }),
-      }),
-    })
-  end
+  -- allows extending the providers array elsewhere in your config
+  -- without having to redefine it
+  opts_extend = { "sources.default" }
 }
